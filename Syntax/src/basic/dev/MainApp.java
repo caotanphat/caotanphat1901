22c6831992ccdb277dc3d65d625088476e32adba
caotanphat <tanphat19012008@gmail.com>
2024-12-17 09:55:24 +0700

    Add files via upload

---
+++ Syntax/src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+/**
+ * Comment nhieu dong
+ */
+// Bai tap 4
+public class MainApp {
+public static void main (String[] args ) {
+	//Comment tren 1 dong cach thu nhat
+	/* Comment tren 1 dong cach thu 2 */
+	String Hueic;
+	int orderNumber;
+	double PI = 3.14;
+}
+public void themSanPham() {
+}
+		public double tinhTienLuong() {
+			return 0;
+}
+}
