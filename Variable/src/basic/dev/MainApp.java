c1821a3c878657c7753d06ad027ad16d0d9c1701
caotanphat <tanphat19012008@gmail.com>
2024-12-17 09:56:02 +0700

    Add files via upload

---
+++ Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,26 @@
+package basic.dev;
+//Bai tap 5
+	/**
+	 *
+	 */
+public class MainApp {
+	public static void main (String[] args) {
+		/**
+		 * khai bao bien
+		 */
+		String firstName = "Cao Tan";
+		String lastName = "Phat";
+		int old = 16;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Cao Tan Phat:");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+}
+
+}
